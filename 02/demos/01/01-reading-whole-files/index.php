652a73caf41c7abe33bff13f0e9d6c40948dba9e
Karolina Redden <dubajkaro@gmail.com>
2023-02-27 16:37:53 -0400

    add read functions to read a book

--- 02/demos/01/01-reading-whole-files/index.php
+++ 02/demos/01/01-reading-whole-files/index.php
@@ -1,9 +1,9 @@
 <?php
-    $filename = "../../../books/Charles Dickens/A Tale of Two Cities.txt"; 
+    $filename = "../../../demos/books/Charles Dickens/A Tale of Two Cities.txt"; 
 
     // uncomment one of the statements below to print out the book's text
 
     // print(file_get_contents($filename));
-    // readfile($filename);
+    readfile($filename);
     // print_r(file($filename));
 ?>
--- 02/index.php
+++ 02/index.php
@@ -1,3 +1,8 @@
 <?php
-  $filename = './books/Charles Dickens/A Tale of Two Cities.txt';
+  $filename = "./demos/books/Charles Dickens/A Tale of Two Cities.txt"; 
+
+
+  print(file_get_contents($filename));
+  // readfile($filename);
+  // print_r(file($filename));
 ?>
