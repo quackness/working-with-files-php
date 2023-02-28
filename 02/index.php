ee77f97da7281de7fb84ba6766b365f59af6391b
Karolina Redden <dubajkaro@gmail.com>
2023-02-27 20:19:42 -0400

    add read one line at a time

--- 02/index.php
+++ 02/index.php
@@ -1,8 +1,17 @@
 <?php
   $filename = "./demos/books/Charles Dickens/A Tale of Two Cities.txt"; 
 
-
-  print(file_get_contents($filename));
+  // 1. Read the file options in PHP
+  // print(file_get_contents($filename));
   // readfile($filename);
   // print_r(file($filename));
+
+//2. Reading partial files: open a file with fopen(filename or path, access mode req. args)
+  $f = fopen($filename, 'r');
+
+  while ($data = fgets($f)) {
+    echo $data;
+  }
+  fclose($f);
+
 ?>
