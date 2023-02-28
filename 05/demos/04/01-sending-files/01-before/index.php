d37f172177824bd82ab3e42dbd4bd095ec888815
Karolina Redden <dubajkaro@gmail.com>
2023-02-28 10:57:56 -0400

    send files to php, opened it in a browser top test

--- 05/demos/04/01-sending-files/01-before/index.php
+++ 05/demos/04/01-sending-files/01-before/index.php
@@ -1,5 +1,7 @@
+
 <?php
     $filename = "../../../books/Charles Dickens/A Tale of Two Cities.txt"; 
-
+    //send files in php 
+    header("content-type: text/plain");
     readfile($filename);
 ?>
